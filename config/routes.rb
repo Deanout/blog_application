a228c05c7fbc17a30e4d48e34f5bbb36bfafc5bf
Dean DeHart <DeHartDean@gmail.com>
2022-01-31 02:13:43 -0500

    Adding The Posts

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  resources :posts
   get 'about', to: 'pages#about'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,7 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+10.times do |x|
+  Post.create(title: "Title #{x}", body: "Body #{x} Words go here Idk")
+end
