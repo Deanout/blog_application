4ebcaf359dd53474e12361ddd7a0b289d6753c3f
Dean DeHart <DeHartDean@gmail.com>
2022-01-31 01:37:35 -0500

    Init commit

---
+++ test/controllers/pages_controller_test.rb
@@ -0,0 +1,13 @@
+require "test_helper"
+
+class PagesControllerTest < ActionDispatch::IntegrationTest
+  test "should get home" do
+    get pages_home_url
+    assert_response :success
+  end
+
+  test "should get about" do
+    get pages_about_url
+    assert_response :success
+  end
+end
