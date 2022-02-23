fbe96ab7db952c07489d512c2ef8bac262c86b56
Dean DeHart <DeHartDean@gmail.com>
2022-02-23 01:48:16 -0500

    Episode 4 commit

---
+++ app/javascript/controllers/comments_controller.js
@@ -0,0 +1,18 @@
+import { Controller } from "@hotwired/stimulus";
+
+export default class extends Controller {
+  initialize() {}
+  connect() {}
+  toggleForm(event) {
+    console.log("I clicked the edit button.");
+    event.preventDefault();
+    event.stopPropagation();
+    const formID = event.params["form"];
+    const commentBodyID = event.params["body"];
+    const form = document.getElementById(formID);
+    form.classList.toggle("d-none");
+    form.classList.toggle("mt-5");
+    const commentBody = document.getElementById(commentBodyID);
+    commentBody.classList.toggle("d-none");
+  }
+}
